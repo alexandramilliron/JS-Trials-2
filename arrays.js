ffebe81af3f2d9ac74b9de8d5316f9ee14b26c90
alexandramilliron <alexandramilliron@gmail.com>
2020-10-20 17:51:08 -0700

    completed arrays.js and functions.js

--- arrays.js
+++ arrays.js
@@ -3,17 +3,29 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`)
+  }
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = []
+  for (const i in items) {
+    if (i % 2 == 0) {
+      result.push(items[i]);
+    }
+  }
+  return result
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  items.sort((a, b) => a - b);
+  const slice = items.slice(0, n);
+  slice.reverse();
+
+  return slice
 }
--- functions.js
+++ functions.js
@@ -1,16 +1,39 @@
 "use strict";
 
 
-// 1. isHometown
+function isHometown(town) {
+    if (town === 'San Francisco') {
+        return true;
+    } else {
+        return false; 
+    }
+}
 
-// Define your function here
+
+function getFullName(first, last) {
+    return (`${first} ${last}`);
+}
 
 
-// 2. getFullName
+function calculateTotal(base, state, tax = 0.05) {
 
-// Define your function here
+    const subtotal = base * (1 + tax);
 
+    let fee = 0;
+    
+    if (state === 'CA') {
+        fee = 0.03 * subtotal; 
+    } else if (state === 'PA') {
+        fee = 2;
+    } else (state === 'MA') 
+    {
+        if (base <= 100) {
+            fee = 1;
+        } else {
+            fee = 3;
+        }
+    }
 
-// 3. calculateTotal
+    return (subtotal + fee);
+}
 
-// Define your function here
